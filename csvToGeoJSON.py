f28a383588a15bb851c8b5f04c4f584fe7958da6
TLeonBrown <tianbrown@gmail.com>
2020-08-29 11:53:06 -0400

    Update csvToGeoJSON file.

--- csvToGeoJSON.py
+++ csvToGeoJSON.py
@@ -2,6 +2,7 @@ import csv
 import json
 
 
+# Store the relevant geographical data for a NY county.
 class NYCounty():
 
 	def __init__(self, countyName, countyLSAD, countyCensusArea, countyCoordinates, fipsID):
@@ -12,17 +13,31 @@ class NYCounty():
 		self.fipsID = fipsID;
 
 
+# Convert the data in a NYCounty object into an element in a .json.
 def countyToJSON(county):
-
+	jsonDict = {
+	'properties': {
+		'name': county.countyName,
+		'LSAD': county.countyLSAD,
+		'censusArea': county.countyCensusArea,
+		},
+	'geometry': {
+		'coordinates': county.countyCoordinates,
+		},
+	'id': county.fipsID,
+	}
+	
+	nyDataFinalDict[county.countyName] = jsonDict
 
 
 # Open .json containing county data for all counties in USA. 
 # Taken from: https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json
-with open('allCountiesUSAData.json', 'r') as allCountiesUSAData:
+with open('data/geojson-counties-fips.json', 'r') as allCountiesUSAData:
 	d = allCountiesUSAData.read()
 
 rawData = json.loads(d)
 nyData = []
+filteredData = {}
 
 # Filter out all non-NY counties.
 for d in rawData['features']:
@@ -33,30 +48,8 @@ for d in rawData['features']:
 for c in nyData:
 	cProperties = c['properties']
 	cGeometry = c['geometry']
-	county = NYCounty(cProperties['NAME'], cProperties['LSAD'], cProperties['CENSUSAREA'], cGeometry['coordinates'], c['id'])
+	countyToJSON(NYCounty(cProperties['NAME'], cProperties['LSAD'], cProperties['CENSUSAREA'], cGeometry['coordinates'], c['id']))
 
-
-jsonFormat = ("{"
-	"'type': 'Feature', "
-	"'properties': {"
-  		"'COUNTY': countyNumber,"
-  		"'NAME': 'Autauga',"
-  		"'LSAD': 'County',"
-  		"'CENSUSAREA': 594.436},"
- 	"'geometry': {"
- 		"'type': 'Polygon',"
-  		"'coordinates': [["
-  			"[-86.496774, 32.344437],"
-    		"[-86.717897, 32.402814],"
-    		"[-86.814912, 32.340803],"
-    		"[-86.890581, 32.502974],"
-    		"[-86.917595, 32.664169],"
-    		"[-86.71339, 32.661732],"
-    		"[-86.714219, 32.705694],"
-    		"[-86.413116, 32.707386],"
-    		"[-86.411172, 32.409937],"
-    		"[-86.496774, 32.344437]]]},"
- 	"'id': '01001'"
-"}")
-
-# print(jsonFormat)
+# Export the completed dict to a .json file.
+with open ('data/nyCountyGeoData.json', 'w') as jsonFile:
+		json.dump(filteredData, jsonFile)
