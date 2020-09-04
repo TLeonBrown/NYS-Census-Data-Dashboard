8cc18e19236e5d727ca5ed3c471070ef9a93a302
TLeonBrown <tianbrown@gmail.com>
2020-09-04 15:32:06 -0400

    Add in file to initialize csv data for all counties.

--- main.py
+++ main.py
@@ -2,38 +2,7 @@ import csv
 import plotly.graph_objects as go
 import numpy as np
 
-
-# Method for creating a County object from raw .csv data.
-def createCounty(data, rowIndex):
-	countyData = []
-	for row in data:
-		countyData.append(row[rowIndex])
-	return County(countyData)
-
-
-# Object for each county's .csv data.
-class County():
-
-	def __init__(self, countyData):
-		# Relevant statistics shown in the .csv file.
-		self.countyName = countyData[0][0:countyData[0].find(',')]
-		self.population2019 = countyData[1]
-		self.population2010 = countyData[2]
-		self.personsUnder5 = countyData[5]
-		self.personsUnder18 = countyData[6]
-		self.personsOver65 = countyData[7]
-		self.sexFemale = countyData[8]
-		self.raceAmericanIndian = countyData[11]
-		self.raceAsian = countyData[12]
-		self.raceBlack = countyData[10]
-		self.raceHispanic = countyData[15]
-		self.raceWhite = countyData[9]
-		self.numHouseHolds = countyData[26]
-		self.commuteTimeMinutes = countyData[44]
-		self.medianHouseholdIncome = countyData[45]
-		self.percentPoverty = countyData[47]
-		self.highSchoolOrHigher = countyData[32]
-		self.bachelorsOrHigher = countyData[33]
+import initializeCountyDict as icd
 
 
 # Dictionary for the .csv data of all counties.
@@ -51,33 +20,7 @@ counties = {
 	'Wyoming': None, 'Yates': None,
 }
 
-# Read all the .csv files and parse counties into a dictionary of County objects.
-with open('data/csv/Albany-Cayuga.csv', 'r') as csvFile:
-	dataAlbanyCayuga = []
-	for row in csv.reader(csvFile):
-		dataAlbanyCayuga.append(row)
-
-	counties['Albany'] = createCounty(dataAlbanyCayuga, 10)
-	counties['Allegany'] = createCounty(dataAlbanyCayuga, 12)
-	counties['Bronx'] = createCounty(dataAlbanyCayuga, 8)
-	counties['Broome'] = createCounty(dataAlbanyCayuga, 6)
-	counties['Cattaraugus'] = createCounty(dataAlbanyCayuga, 4)
-	counties['Cayuga'] = createCounty(dataAlbanyCayuga, 2)
-
-with open('data/csv/Chautauqua-Cortland.csv', 'r') as csvFile:
-	dataChautauquaCortland = []
-	for row in csvFile:
-		dataChautauquaCortland.append(row)
-
-	counties['Chautauqua'] = createCounty(dataChautauquaCortland, 12)
-	counties['Chemung'] = createCounty(dataChautauquaCortland, 10)
-	counties['Chenango'] = createCounty(dataChautauquaCortland, 8)
-	counties['Clinton'] = createCounty(dataChautauquaCortland, 6)
-	counties['Columbia'] = createCounty(dataChautauquaCortland, 4)
-	counties['Cortland'] = createCounty(dataChautauquaCortland, 2)
-
-# TODO: Finish off the rest of the .csv files. 
-
+icd.initializeCounties(counties)
 
 
 # t = np.linspace(5, 25, 300)
